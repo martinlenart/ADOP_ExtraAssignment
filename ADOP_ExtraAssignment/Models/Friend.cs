ff0c6f6180dccc6f0a2809d94bccbdd9599d63ae
Martin <martin_lenart@icloud.com>
2023-02-09 12:05:39 +0100

    Initial Commit

---
+++ ADOP_ExtraAssignment/Models/Friend.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using ADOP_ExtraAssignment.Services;
+
+namespace ADOP_ExtraAssignment.Models
+{
+    public class Friend
+    {
+        public Guid FriendID { get; init; } 
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public override string ToString() => $"{FullName} from {City}, {Country} can be reached at {Email}, from {City}";
+
+        public List<GoodQuote> FavoriteQuotes { get; set; }
+
+        public static class Factory
+        {
+            public static Friend CreateRandom(RandomService rnd)
+            {
+                var fn = rnd.FirstName;
+                var ln = rnd.LastName;
+                var country = rnd.Country;
+
+                var _quotes = new List<GoodQuote>();
+                var _nrQuotes = rnd.Next(0, 3); //none, 1 or 2 favorite quotes 
+                for (int i = 0; i < _nrQuotes; i++)
+                    _quotes.Add(rnd.Quote);
+
+                return new Friend
+                {
+                    FriendID = Guid.NewGuid(),
+
+                    FirstName = fn,
+                    LastName = ln,
+                    Email = rnd.Email(fn, ln),
+                    City = rnd.City(country),
+                    Country = country,
+                    FavoriteQuotes = _quotes
+                 };
+            }
+        }
+    }
+}
---
+++ ADOP_ExtraAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using ADOP_ExtraAssignment.Models;
+using ADOP_ExtraAssignment.Services;
+
+namespace ADOP_ExtraAssignment;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var _randomService = new RandomService();
+
+        //Create 20 random friends
+        var _friends = new List<Friend>();
+        for (int i = 0; i < 20; i++)
+        {
+            _friends.Add(Friend.Factory.CreateRandom(_randomService));
+        }
+
+        //Write out to Console
+        foreach(var friend in _friends)
+        {
+            Console.WriteLine(friend);
+            Console.WriteLine("FavoriteQuotes:");
+            foreach (var quote in friend.FavoriteQuotes)
+            {
+                Console.WriteLine(quote);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
